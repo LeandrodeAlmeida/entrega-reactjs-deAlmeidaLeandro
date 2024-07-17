06aee5306ecb4239535af6781ea3c1c3c67449f8
Leandro de Almeida <dealmeidaleandro14@gmail.com>
2024-07-16 21:48:23 -0300

    agregando firebase

--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { getAllProducts } from "../services/productServices";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+
 
 export const useProducts = () => {
   const [products, setProducts] = React.useState([]);
-
+  const [loading, setLoading] = React.useState([]);
   React.useEffect(() => {
-    getAllProducts()
-      .then((response) => {
-        setProducts(response.data.products);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    const db = getFirestore();
+
+    const productsCollection = collection(db, "products");
+    getDocs(productsCollection).then((snapshot) =>
+    {
+      setProducts(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})))
+    }).catch((error) => console.log (error)).finally(() => setLoading(false))
+   
   }, []);
 
-  return { products };
+  return { products, loading };
 };
